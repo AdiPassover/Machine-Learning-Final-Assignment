1fc2c63bac6fc157371f4f9951049a5231089533
Adi Peisach <95279378+AdiPassover@users.noreply.github.com>
2025-09-08 14:42:05 +0300

    Final additions

--- models/lstm_model.py
+++ models/lstm_model.py
@@ -5,6 +5,7 @@ from keras.src.callbacks import EarlyStopping
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout, Input
 from keras.utils import to_categorical
+from keras.regularizers import l2
 
 class LSTMModel(BaseClassificationModel):
     def __init__(self, input_shape, num_classes, hidden_units=128, dropout=0.3, recurrent_dropout=0.3, lr=0.001, epochs=20, batch_size=32):
@@ -24,12 +25,12 @@ class LSTMModel(BaseClassificationModel):
             Input(shape=self.input_shape),  # Use Input layer here
             LSTM(self.hidden_units, recurrent_dropout=self.recurrent_dropout),
             Dropout(self.dropout),
-            Dense(self.num_classes, activation='softmax', kernel_regularizer=tf.keras.regularizers.l2(0.001))
+            Dense(self.num_classes, activation='softmax', kernel_regularizer=l2(0.001))
         ])
         model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
         return model
 
-    def train(self, X_train, y_train, X_val=None, y_val=None):
+    def train(self, X_train, y_train, X_val=None, y_val=None, verbose=1):
         y_train = to_categorical(y_train, num_classes=self.num_classes)
         if X_val is not None:
             print("Using validation data for training.")
@@ -40,14 +41,14 @@ class LSTMModel(BaseClassificationModel):
                 batch_size=self.batch_size,
                 validation_data=(X_val, y_val) if X_val is not None else None,
                 callbacks=[EarlyStopping(monitor="val_loss", patience=3, restore_best_weights=True)],
-                verbose=1
+                verbose=verbose
             )
         else:
             print("Validation data not provided, training without validation.")
-            self.model.fit(X_train, y_train, epochs=self.epochs, batch_size=self.batch_size, verbose=1)
+            self.model.fit(X_train, y_train, epochs=self.epochs, batch_size=self.batch_size, verbose=verbose)
 
 
-    def predict(self, X_test):
-        preds = self.model.predict(X_test)
+    def predict(self, X_test, verbose=0):
+        preds = self.model.predict(X_test, verbose=verbose)
         return np.argmax(preds, axis=1)
 
